6db4ac561c6fdf1a332470a2dbcbe4cf9b713a26
TecatoKiary <mineevdanis@mail.ru>
2023-01-27 18:24:16 +0300

    add_file

---
+++ search/scale_map.py
@@ -0,0 +1,19 @@
+def scale_map(toponym_object):
+    # Координаты центра топонима:
+    toponym_coodrinates = toponym_object["Point"]["pos"]
+    # Долгота и широта:
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+
+    lower_corner = toponym_object['boundedBy']['Envelope']['lowerCorner'].split()
+    upper_corner = toponym_object['boundedBy']['Envelope']['upperCorner'].split()
+
+    delta_x = abs(float(upper_corner[0]) - float(lower_corner[0]))
+    delta_y = abs(float(upper_corner[1]) - float(lower_corner[1]))
+    print(toponym_object)
+    # Собираем параметры для запроса к StaticMapsAPI:
+    map_params = {
+        "ll": ",".join([toponym_longitude, toponym_lattitude]),
+        "spn": f'{delta_x},{delta_y}',
+        "l": "map"
+    }
+    return map_params
