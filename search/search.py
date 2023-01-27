98ab30bad5e662c293c706f3d0e8ccdef3ae7b65
TecatoKiary <mineevdanis@mail.ru>
2023-01-27 18:23:36 +0300

    full_search

---
+++ search/search.py
@@ -0,0 +1,42 @@
+import sys
+from io import BytesIO
+# Этот класс поможет нам сделать картинку из потока байт
+
+import requests
+from PIL import Image
+
+import scale_map
+
+# Пусть наше приложение предполагает запуск:
+# python search.py Москва, ул. Ак. Королева, 12
+# Тогда запрос к геокодеру формируется следующим образом:
+toponym_to_find = " ".join(sys.argv[1:])
+
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+geocoder_params = {
+    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+    "geocode": toponym_to_find,
+    "format": "json"}
+
+response = requests.get(geocoder_api_server, params=geocoder_params)
+
+if not response:
+    # обработка ошибочной ситуации
+    pass
+
+# Преобразуем ответ в json-объект
+json_response = response.json()
+# Получаем первый топоним из ответа геокодера.
+toponym = json_response["response"]["GeoObjectCollection"][
+    "featureMember"][0]["GeoObject"]
+map_params = scale_map.scale_map(toponym)
+map_params['pt'] = f'{map_params["ll"]},pmrdm99'
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+# ... и выполняем запрос
+response = requests.get(map_api_server, params=map_params)
+
+Image.open(BytesIO(
+    response.content)).show()
+# Создадим картинку
+# и тут же ее покажем встроенным просмотрщиком операционной системы
