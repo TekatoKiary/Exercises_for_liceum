f4cde2135e266e9e53bbb78599b91c99a675e8f1
TekatoKiary <118821189+TekatoKiary@users.noreply.github.com>
2023-03-03 16:31:43 +0300

    fix

--- restful_api_users/test.py
+++ restful_api_users/test.py
@@ -21,8 +21,19 @@ print(post('http://localhost:8080/api/v2/user',
                  'email': 'Mister_Cooper@mars.org',
                  'hashed_password': '498'}).json())
 
+print(post('http://localhost:8080/api/v2/user',
+           json={'surname': 'Rick',
+                 'name': 'Cooper',
+                 'age': 16,
+                 'position': 'assistanc',
+                 'speciality': 'middle engineer',
+                 'address': 'modul_4',
+                 'hashed_password': '498'}).json())  # Некорректный запрос: отсутствует параметр email.
+
 print(get(f'http://localhost:8080/api/v2/user').json())
 
 print(get(f'http://localhost:8080/api/v2/user/1').json())
+
+print(get(f'http://localhost:8080/api/v2/user/3').json())  # Некорректный запрос.
 
 print(delete('http://localhost:8080/api/v2/user/2').json())
